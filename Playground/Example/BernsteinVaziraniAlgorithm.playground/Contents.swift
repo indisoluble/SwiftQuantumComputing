eeedcc01b43fbaa6974dda650bb5d2503b9a0d07
Enrique de la Torre <indisoluble_dev@me.com>
2020-01-25 21:56:18 +0100

    New playground: BernsteinVaziraniAlgorithm

---
+++ Playground/Example/BernsteinVaziraniAlgorithm.playground/Contents.swift
@@ -0,0 +1,19 @@
+import SwiftQuantumComputing // for macOS
+
+let secret = "110"
+
+var gates = [Gate.not(target: 0)]
+gates += Gate.hadamard(targets: 0...secret.count)
+gates += [Gate.oracle(truthTable: makeBernsteinVaziraniTruthTable(secret: secret),
+                      target: 0,
+                      controls: (1...secret.count).reversed())]
+gates += Gate.hadamard(targets: 1...secret.count)
+
+MainDrawerFactory().makeDrawer().drawCircuit(gates)
+
+let circuit = MainCircuitFactory().makeCircuit(gates: gates)
+let probabilities = circuit.summarizedProbabilities(qubits: (1...secret.count).reversed())
+
+let foundSecret = probabilities.keys.first!
+
+print("Secret: \(secret). Found secret: \(foundSecret). Success? \(secret == foundSecret)")
