f74f3bdae308b80876fc1a515dddf0050113f359
Enrique de la Torre <indisoluble_dev@me.com>
2020-03-10 00:08:17 +0100

    New extension: Gate.makeModularExponentiation(base:modulus:exponent:inputs:) + ranges

--- Tests/SwiftQuantumComputingTests/XCTestManifests.swift
+++ Tests/SwiftQuantumComputingTests/XCTestManifests.swift
@@ -62,6 +62,7 @@ public func allTests() -> [XCTestCaseEntry] {
         testCase(Gate_InversionAboutMeanTests.allTests),
         testCase(Gate_ModularExponentiationTests.allTests),
         testCase(Gate_InversionAboutMeanWithRangeInputsTests.allTests),
+        testCase(Gate_ModularExponentiationWithRangeExponentAndInputsTests.allTests),
         testCase(MainGeneticCircuitEvaluatorTests.allTests),
         testCase(MainGeneticUseCaseEvaluatorTests.allTests),
         testCase(ControlledMatrixGateTests.allTests),
