77a5a81a6bfafaa381fcf4f77b2527c8362f33f5
Enrique de la Torre <indisoluble_dev@me.com>
2020-09-19 16:30:43 +0200

    Inc. swift-numerics version to 0.0.8

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 var dependencies: [Package.Dependency] = [
     .package(
         url: "https://github.com/apple/swift-numerics.git",
-        .exact("0.0.7")
+        .exact("0.0.8")
     )
 ]
 #if os(Linux)
