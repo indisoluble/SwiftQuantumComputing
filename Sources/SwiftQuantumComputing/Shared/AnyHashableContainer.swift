97afd7f4538dc5da5aabd4287866eecd030fdace
Enrique de la Torre <indisoluble_dev@me.com>
2020-11-08 15:20:57 +0100

    Define AnyHashableContainer

---
+++ Sources/SwiftQuantumComputing/Shared/AnyHashableContainer.swift
@@ -0,0 +1,39 @@
+//
+//  AnyHashableContainer.swift
+//  SwiftQuantumComputing
+//
+//  Created by Enrique de la Torre on 08/11/2020.
+//  Copyright © 2020 Enrique de la Torre. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+
+// MARK: - Protocol definition
+
+public protocol AnyHashableContainer {
+    var anyHash: AnyHashable { get }
+}
+
+// MARK: - Hashable default implementations
+
+extension Hashable where Self: AnyHashableContainer {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.anyHash == rhs.anyHash
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        anyHash.hash(into: &hasher)
+    }
+}
