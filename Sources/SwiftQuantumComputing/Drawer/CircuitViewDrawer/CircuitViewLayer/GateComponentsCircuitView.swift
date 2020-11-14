391e22f1b8c391d9aee512e0e129c82f8192d35b
Enrique de la Torre <indisoluble_dev@me.com>
2020-11-14 20:51:39 +0100

    Define CircuitViewLayer, GateComponentsCircuitView & SingleQubitGateCircuitView

---
+++ Sources/SwiftQuantumComputing/Drawer/CircuitViewDrawer/CircuitViewLayer/GateComponentsCircuitView.swift
@@ -0,0 +1,53 @@
+//
+//  GateComponentsCircuitView.swift
+//  SwiftQuantumComputing
+//
+//  Created by Enrique de la Torre on 14/11/2020.
+//  Copyright © 2020 Enrique de la Torre. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+
+// MARK: - Internal types
+
+enum GateCircuitView {
+    case singleQubit(gate: SingleQubitGateCircuitView)
+    case multiQubit(inputs: [Int])
+
+    var inputs: [Int] {
+        switch self {
+        case .singleQubit(let gate):
+            return [gate.target]
+        case .multiQubit(let inputs):
+            return inputs
+        }
+    }
+}
+
+// MARK: - Protocol definition
+
+protocol GateComponentsCircuitView {
+    typealias GateComponents = (controls: [Int], oracleControls: [Int], gate: GateCircuitView)
+
+    func extractComponents() -> GateComponents
+}
+
+// MARK: - GateComponentsCircuitView default implementations
+
+extension GateComponentsCircuitView where Self: SingleQubitGateCircuitView {
+    func extractComponents() -> GateComponents {
+        return ([], [], .singleQubit(gate: self))
+    }
+}
