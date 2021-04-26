ede932417f1da7a9e67dd710fd0dafbf9caa4548
Enrique de la Torre <indisoluble_dev@me.com>
2021-04-26 21:39:05 +0200

    Circuit.playground: simplify

--- Playground/Usage/Drawer.playground/Contents.swift
+++ Playground/Usage/Drawer.playground/Contents.swift
@@ -1,24 +1,15 @@
 import SwiftQuantumComputing // for macOS
-//: 1. Compose a list of quantum gates. Insert them in the same order
-//:    you want them to appear in the quantum circuit
-let matrix = Matrix([[.one, .zero, .zero, .zero],
-                     [.zero, .one, .zero, .zero],
-                     [.zero, .zero, .zero, .one],
-                     [.zero, .zero, .one, .zero]])
+//: 1. Compose a list of quantum gates. Insert them in the same order you want them to appear in the quantum circuit
 let gates: [Gate] = [
     .not(target: 0),
     .hadamard(target: 1),
     .phaseShift(radians: 0.25, target: 2),
     .rotation(axis: .z, radians: 1, target: 3),
-    .matrix(matrix: matrix, inputs: [3, 2]),
-    .matrix(matrix: matrix, inputs: [0, 3]),
+    .matrix(matrix: Matrix([[.zero, .one], [.one, .zero]]), inputs: [4]),
     .oracle(truthTable: ["01", "10"],
             controls: [0, 1],
             gate: .rotation(axis: .x, radians: 0.5, target: 3)),
-    .oracle(truthTable: ["0"], controls: [0], target: 2),
-    .controlled(gate: .hadamard(target: 4), controls: [2]),
-    .controlled(gate: .matrix(matrix: matrix, inputs: [4, 2]), controls: [1, 0]),
-    .controlledNot(target: 0, control: 3)
+    .controlled(gate: .hadamard(target: 4), controls: [2, 0])
 ]
 //: 2. Draw the quantum circuit to see how it looks
 let drawer = MainDrawerFactory().makeDrawer()
