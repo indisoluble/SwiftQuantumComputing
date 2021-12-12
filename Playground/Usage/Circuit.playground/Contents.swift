70ef769de79fea3fcc3bb1abdb29524e5e0f8039
Enrique de la Torre <indisoluble_dev@me.com>
2021-12-12 16:28:43 +0100

    Update playgrounds

--- Playground/Usage/Circuit.playground/Contents.swift
+++ Playground/Usage/Circuit.playground/Contents.swift
@@ -5,7 +5,7 @@ let gates: [Gate] = [
     .hadamard(target: 1),
     .phaseShift(radians: 0.25, target: 2),
     .rotation(axis: .z, radians: 1, target: 3),
-    .matrix(matrix: Matrix([[.zero, .one], [.one, .zero]]), inputs: [4]),
+    .matrix(matrix: try Matrix([[.zero, .one], [.one, .zero]]), inputs: [4]),
     .oracle(truthTable: ["01", "10"],
             controls: [0, 1],
             gate: .rotation(axis: .x, radians: 0.5, target: 3)),
@@ -13,16 +13,11 @@ let gates: [Gate] = [
 ]
 //: 2. Draw the quantum circuit to see how it looks
 let drawer = MainDrawerFactory().makeDrawer()
-drawer.drawCircuit(gates).get()
+try drawer.drawCircuit(gates).get()
 //: 3. Build the quantum circuit with the list of gates
 let circuit = MainCircuitFactory().makeCircuit(gates: gates)
 //: 4. Use the quantum circuit
-let result = circuit.statevector().get()
+let result = try circuit.statevector().get()
 print("Statevector: \(result)\n")
-print("Density matrix: \(result.densityMatrix())\n")
 print("Probabilities: \(result.probabilities())\n")
-print("Summarized probabilities: \(result.summarizedProbabilities())\n")
-let grpProbs = result.groupedProbabilities(byQubits: [1, 0],
-                                           summarizedByQubits: [4, 3, 2]).get()
-print("Grouped probabilities: \(grpProbs)")
-print("Unitary: \(circuit.unitary().get())\n")
+print("Unitary: \(try circuit.unitary().get())\n")
--- Playground/Usage/TwoLevelDecomposition.playground/Contents.swift
+++ Playground/Usage/TwoLevelDecomposition.playground/Contents.swift
@@ -18,23 +18,23 @@ let gates = [
                 gate: .phaseShift(radians: 0.25, target: 10))
 ]
 //: 2. (Optional) Draw gates to see how they look
-drawer.drawCircuit(gates).get()
+try drawer.drawCircuit(gates).get()
 //: 3. Build circuit and measure how long it takes to get the statevector
 var start = CFAbsoluteTimeGetCurrent()
-circuitFactory.makeCircuit(gates: gates).statevector().get()
+try circuitFactory.makeCircuit(gates: gates).statevector().get()
 var diff = CFAbsoluteTimeGetCurrent() - start
 print("Original circuit executed in \(diff) seconds")
 //: 4. Decompose gates into an equivalent sequence of fully controlled matrix gates and not gates
 start = CFAbsoluteTimeGetCurrent()
-let decomposition = solver.decomposeGates(gates).get()
+let decomposition = try solver.decomposeGates(gates).get()
 diff = CFAbsoluteTimeGetCurrent() - start
 print("Original circuit decomposed in \(diff) seconds")
 //: 5. (Optional) Draw decomposition to see how it looks
-drawer.drawCircuit(decomposition).get()
+try drawer.drawCircuit(decomposition).get()
 //: 6. Build a new circuit and measure how long it takes to get the statevector.
 //: Single qubit gates & fully controlled gates are faster to execute, however this
 //: descomposition produces a lot of them
 start = CFAbsoluteTimeGetCurrent()
-circuitFactory.makeCircuit(gates: decomposition).statevector().get()
+try circuitFactory.makeCircuit(gates: decomposition).statevector().get()
 diff = CFAbsoluteTimeGetCurrent() - start
 print("Decomposed circuit executed in \(diff) seconds")
